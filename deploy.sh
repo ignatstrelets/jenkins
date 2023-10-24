664b1c8d5665bee1f8f727e83b19defaadaeda83
ignatstrelets <ubuntu@ip-172-31-27-214.eu-north-1.compute.internal>
2023-10-24 16:37:38 +0000

    test

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,6 @@ echo "Starting to deploy docker image.."
 CURRENT_IMAGE=$DOCKER_REPO/$DOCKER_IMAGE:latest
 docker pull $CURRENT_IMAGE
 docker ps -q --filter ancestor=$DOCKER_IMAGE | xargs -r docker stop
-docker run -d -p $APP_PORT:$APP_PORT $CURRENT_IMAGE
+echo $APP_PORT
+echo $CURRENT_IMAGE
+docker run -d -p $APP_PORT:$APP_PORT $CURRENT_IMAGE -v
