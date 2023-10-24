5dfa481969069255cf13500eea1c9eb9e0954f28
ignatstrelets <ubuntu@ip-172-31-27-214.eu-north-1.compute.internal>
2023-10-24 16:25:54 +0000

    edit Dockerfile

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "Starting to deploy docker image.."
-docker pull $DOCKER_REPO/$DOCKER_IMAGE:latest
+CURRENT_IMAGE=$DOCKER_REPO/$DOCKER_IMAGE:latest
+docker pull $CURRENT_IMAGE
 docker ps -q --filter ancestor=$DOCKER_IMAGE | xargs -r docker stop
-docker run -d -p $APP_PORT:$APP_PORT $DOCKER_IMAGE
+docker run -d -p $APP_PORT:$APP_PORT $CURRENT_IMAGE
